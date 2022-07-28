3de81fa78479e11420533e9d37f329dc03d8691f
Node0 <business@node-0.com>
2022-07-28 16:16:11 -0700

    Added DS_Store file removal

--- joomla-builder/jBuilder.py
+++ joomla-builder/jBuilder.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import os, sh, shutil
-# Please ensure sh library is installed: pip3 install sh
 
-class JBuilder():
+class Jbuilder():
   def __init__(self):
-    print("Running with assumption that jBuilder.py is run from the 'joomla-builder' folder.\nif this is not the case please move jBuilder.py to the 'joomla-builder' folder and run it again.")
+    print("Assuming jBuilder.py is run from the 'joomla-builder' folder, if this is not the case please move jBuilder.py to the 'built-tools' folder and run it again.")
     os.chdir("../")
     self.basePath = os.getcwd()
     self.srcDirs = [ 'components', 'plugins', 'modules', 'libraries' ]
@@ -14,7 +13,8 @@ class JBuilder():
     self.lineSepThick = '==========================================================================='
     self.lineSepThin  = '---------------------------------------------------------------------------'
 
-    # Ensure that all dirs listed in srcDirs actually exist, else exit with error
+    # If ANY of the core extension-type source dirs specified in array above do not exist on the filesystem
+    # exit with error message, without source folder mappings we cannot reliably populate ./builds with artifacts.
     for srcDir in self.srcDirs:
       if ( os.path.exists( srcDir ) ):
         print( f"Source directory {self.basePath}/{srcDir} exists" )
@@ -22,31 +22,11 @@ class JBuilder():
         print( f"ERROR The directory {self.basePath}/{srcDir} does not exist, EXITING." )
         exit(1)
 
-
-
-  # Returns a list of folders one level deep from location where it's called
-  def getTopLevelDirs(self, folder):
-    return [d for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
-
-  def packageDirsInSrcDirs(self):
-    # For each extension folder type listed in self.srcDirs array
-    for srcDir in self.srcDirs:
-      if ( os.path.exists( f"{self.basePath}/{srcDir}" ) ):
-        os.chdir( f"{self.basePath}/{srcDir}" )
-        print(f"{self.lineSepThin}")
-        print( f"Working in {os.getcwd()}..." )
-        # Check for folders in this extension folder type (one level deep, assume each folder is a separate extension)
-        for extension in self.getTopLevelDirs( f"{self.basePath}/{srcDir}" ):
-          sh.zip( "-r", f"{extension}.zip", f"{extension}" )
-          sh.mv( f"{self.basePath}/{srcDir}/{extension}.zip", f"{self.buildsDir}"  )
-          print(f"Built {extension} as {extension}.zip, you can find it in the ./builds folder.")
-  # Create and clean build dir
-  def createBuildsFolder(self):
+    # Create and clean build dir
     if ( not os.path.exists(self.buildsDir) ):
       sh.mkdir("-p", self.buildsDir)
       os.chdir( self.buildsDir )
-      print( f"{os.getcwd()} now exists and is ready!" )
-      print(f"{self.lineSepThick}\n\n")
+      print( f"{os.getcwd()} now exists" )
     else:
       os.chdir( self.buildsDir )
       thisDir = os.getcwd()
@@ -56,12 +36,39 @@ class JBuilder():
             shutil.rmtree( path )
         except OSError:
             os.remove( path )
-      print( f"{os.getcwd()} now cleaned and is ready!" )
-      print(f"{self.lineSepThick}\n\n")
+      print( f"{os.getcwd()} is now cleaned and ready" )
+      print(f"{self.lineSepThick}")
+
+
+  def listdirs(self, folder):
+    return [d for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
+
+  def removeDsStoreFiles(self):
+    findDsStoreFilesStdout = sh.find(f"{self.basePath}", "-type", "f", "-name", ".DS_Store",  _iter=True )
+    foundDsStoreFiles = findDsStoreFilesStdout.split('\n')
+    foundDsStoreFiles.pop()
+    if ( len(foundDsStoreFiles) > 0 ):
+      print("Removing .DS_Store files...")
+      for dsStoreFile in foundDsStoreFiles:
+        sh.rm("-f", dsStoreFile )
+        print(f"Removed {dsStoreFile}")
+
+  def packageDirsInSrcDirs(self):
+    for srcDir in self.srcDirs:
+      if ( os.path.exists( f"{self.basePath}/{srcDir}" ) ):
+        os.chdir( f"{self.basePath}/{srcDir}" )
+        print(f"{self.lineSepThin}")
+        print( f"Working in {os.getcwd()}" )
+        # print( self.listdirs( f"{self.basePath}/{srcDir}" ) )
+        for extension in self.listdirs( f"{self.basePath}/{srcDir}" ):
+          sh.zip( "-r", f"{extension}.zip", f"{extension}" )
+          sh.mv( f"{self.basePath}/{srcDir}/{extension}.zip", f"{self.buildsDir}"  )
+          print(f"Packaged {extension} -> {extension}.zip and moved to ./builds folder. ")
 
   def execute(self):
-    self.createBuildsFolder()
+    self.removeDsStoreFiles()
     self.packageDirsInSrcDirs()
 
-JB = JBuilder()
+JB = Jbuilder()
 JB.execute()
+
